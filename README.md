702735bd21e306f2abb3b89312bb82ba39f3a9c5
Juan Manuel Berros <juanmaberros@gmail.com>
2016-07-06 15:17:25 -0300

    update readme mentioning grip latest version

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Add this repo to your `$PATH` to use the scripts in the command line.
 
 ## Convert a Jupyter Notebook to a nice HTML version
-You need python 3.5 and `pip install grip`. I assume you already have `jupyter` installed if you're dealing with notebooks.
+You need python 3.5 and the lastest version of `grip` (`pip install git+https://github.com/joeyespo/grip`). I assume you already have `jupyter` installed if you're dealing with notebooks.
 
 `convert_notebook_to_a_nice_HTML.py NOTEBOOK_PATH`
 
-The script will create both a markdown (`.md`) and an HTML version of the notebook in the same directory and with the same filename (and different extension, obvs).
+The script will create both a markdown (`.md`) version of the notebook using `jupyter nbconvert --to markdown`, and then a nicely formatted HTML version using `grip --export`. Files will be located in the same directory and with the same basename.
--- convert_notebook_to_a_nice_HTML.py
+++ convert_notebook_to_a_nice_HTML.py
@@ -2,34 +2,59 @@
 """This script will create a nice HTML version of your Jupyter notebook.
 It needs Python grip installed (pip install grip).
 
-Usage:
-    convert_notebook_to_nice_HTML.py NOTEBOOK_PATH
+Usage:
+    convert_notebook_to_nice_HTML.py NOTEBOOK_PATH [options]
 
+Options:
+    -h --help       Show this screen.
+    --md-exists     Don't convert to markdown, assume the .md file is already
+                    there.
+    --html-exists   Don't use grip to convert to HTML, assume the HTML is
+                    already there.
 """
 from subprocess import run
 from docopt import docopt
 
 
 def main(arguments):
-    # Create the markdown version of the notebook
     notebook = arguments['NOTEBOOK_PATH']
-    command = 'jupyter nbconvert --to markdown %s' % notebook
-    print('\n*', command)
-    run(command.split(), check=True)
+    if not arguments['--md-exists']:
+        # Create the markdown version of the notebook
+        command = 'jupyter nbconvert --to markdown %s' % notebook
+        print('\n*', command)
+        run(command.split(), check=True)
 
-    # Export the markdown version to HTML
     markdown = notebook.replace('.ipynb', '.md')
-    command = 'grip --export %s' % markdown
-    print('\n*', command)
-    run(command.split(), check=True)
+    if not arguments['--html-exists']:
+        # Export the markdown version to HTML
+        command = 'grip --export %s' % markdown
+        print('\n*', command)
+        run(command.split(), check=True)
 
     # Replace the stylesheet
     html = markdown.replace('.md', '.html')
-    stylesheet_tag = '<style>body .container {width: 97%;}</style>'
+    stylesheet_tag = """
+<style>
+    body .container {width: 97%;}
+    body .markdown-body table { display: table; border: 2px solid silver; width: auto; font-size: 0.8em; line-height: 1; }
+    body .markdown-body table th, .markdown-body table td { padding: 5px; }
+    img { max-width: 1200px !important; }
+</style>
+""".replace('\n', ' ')
     replace_pattern = "s#<link rel=\"stylesheet\" href=\"//octicons.github.com/components/octicons/octicons/octicons.css\" />#%s#" % stylesheet_tag
     command_args = ['sed', '-i', replace_pattern, html]
     print('\n*', ' '.join(command_args))
     run(command_args, check=True)
+
+    # Centered vertical alignment for combined cells in tables
+    replace_pattern = 's/valign="top"/valign="center"/'
+    command_args = ['sed', '-i', replace_pattern, html]
+    print('\n*', ' '.join(command_args))
+    run(command_args, check=True)
+
+    print()
+    print('Done!\n')
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
